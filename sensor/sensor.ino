b2d2445892c824047871ed03b71fcf66bf98ca42
Levi Lesches <levilesches@gmail.com>
2019-07-15 13:07:50 -0400

    Comments

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,17 +1,24 @@
+#define BUILTIN A0
+
 #define ECHO A3
 #define TRIGGER A2
 #define THRESHOLD 6
 
 bool isApproaching() {
+	// Flush the trigger's power
 	digitalWrite(TRIGGER, LOW);
 	delayMicroseconds(2);
 
+	// Trigger a 10 micro-second sound wave pulse
 	digitalWrite(TRIGGER, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(TRIGGER, LOW);
 
+	// Calculate the distance based on the incoming sound wave
+	// I'm relatively sure this is in cm
 	long distance = pulseIn(ECHO, HIGH) * 0.034 / 2;
 
+	// Determine whether soemthing is approaching
 	return distance < THRESHOLD;
 }
 
@@ -29,39 +36,3 @@ void loop() {
 	} else digitalWrite(9, LOW);
 	delay (200);
 }
-
-
-/*
-* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
-*
-* by Dejan Nedelkovski,
-* www.HowToMechatronics.com
-*
-*/
-// defines pins numbers
-// const int trigPin = 9;
-// const int echoPin = 10;
-// // defines variables
-// long duration;
-// int distance;
-// void setup() {
-// pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-// pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-// Serial.begin(9600); // Starts the serial communication
-// }
-// void loop() {
-// // Clears the trigPin
-// digitalWrite(trigPin, LOW);
-// delayMicroseconds(2);
-// // Sets the trigPin on HIGH state for 10 micro seconds
-// digitalWrite(trigPin, HIGH);
-// delayMicroseconds(10);
-// digitalWrite(trigPin, LOW);
-// // Reads the echoPin, returns the sound wave travel time in microseconds
-// duration = pulseIn(echoPin, HIGH);
-// // Calculating the distance
-// distance= duration*0.034/2;
-// // Prints the distance on the Serial Monitor
-// Serial.print("Distance: ");
-// Serial.println(distance);
-// }
--- servos/servos.ino
+++ servos/servos.ino
@@ -17,44 +17,28 @@ float getPulse (int angle) {
 }
 
 void setup() {
-	Serial.begin (9600);
 	pivot.attach (PIVOT);
 	lift.attach (LIFT);
 	pinMode (13, OUTPUT);
-	Serial.print("Slope is: ");
-	Serial.println(slope);
 }
 
 void loop() {
-	// for (int angle = 0; angle < 181; angle+=10) {
-	// 	digitalWrite (13, HIGH);
-	// 	float pulse = (slope * angle) + intercept;
-	// 	Serial.print("Writing to ");
-	// 	Serial.print(angle);
-	// 	Serial.print (", which is ");
-	// 	Serial.println (pulse);
-	// 	pivot.write (pulse);
-	// 	lift.write (pulse);
-	// 	digitalWrite(13, LOW);
-	// 	delay (10);
-	// }
-	float pulse;
 	digitalWrite(13, HIGH);
+	float pulse;
+	// Go to 0
 	pulse = getPulse(0);
 	pivot.write (pulse);
 	lift.write (pulse);
-	delay (1000);
+	delay (1000);  // break from 180 to 0
+	// Go to 90
 	pulse = getPulse(90);
 	pivot.write (pulse);
 	lift.write (pulse);
 	delay (500);	
+	// Go to 180
 	pulse = getPulse(180);
 	pivot.write (pulse);
 	lift.write (pulse);
-	digitalWrite(13, LOW);
-	delay (1000);	
+	digitalWrite(13, LOW);  // done
+	delay (500);	
 }
-
-// 0 -> 750
-// 90 -> 1500
-// 180 -> 2250
