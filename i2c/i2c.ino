9f74e862517ccc58c3db99e8bf8805b8a3a8895c
Levi Lesches <levilesches@gmail.com>
2019-07-31 13:48:54 -0400

    Commented `i2c.ino`

--- i2c/i2c.ino
+++ i2c/i2c.ino
@@ -1,66 +1,45 @@
-// --------------------------------------
-// i2c_scanner
-//
-// Version 1
-//    This program (or code that looks like it)
-//    can be found in many places.
-//    For example on the Arduino.cc forum.
-//    The original author is not know.
-// Version 2, Juni 2012, Using Arduino 1.0.1
-//     Adapted to be as simple as possible by Arduino.cc user Krodal
-// Version 3, Feb 26  2013
-//    V3 by louarnold
-// Version 4, March 3, 2013, Using Arduino 1.0.3
-//    by Arduino.cc user Krodal.
-//    Changes by louarnold removed.
-//    Scanning addresses changed from 0...127 to 1...119,
-//    according to the i2c scanner by Nick Gammon
-//    https://www.gammon.com.au/forum/?id=10896
-// Version 5, March 28, 2013
-//    As version 4, but address scans now to 127.
-//    A sensor seems to use address 120.
-// Version 6, November 27, 2015.
-//    Added waiting for the Leonardo serial communication.
-//
-//
-// This sketch tests the standard 7-bit addresses
-// Devices with higher bit address might not be seen properly.
-//
- 
+/*
+  This sketch lists all I2C devices it can find
+  It was taken from online, and I do not claim it to be my own
+  Helpful for debugging any I2C device
+
+  Specs:
+    Program storage: 11%
+    Dynamic memory: 26%
+*/
+
 #include <Wire.h>
  
- 
-void setup()
-{
+void setup() {
   Wire.begin();
  
   Serial.begin(9600);
-  while (!Serial);             // Leonardo: wait for serial monitor
   Serial.println("\nI2C Scanner");
 }
  
  
-void loop()
-{
+void loop() {
   byte error, address;
   int nDevices;
  
   Serial.println("Scanning...");
  
   nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
+  for(address = 1; address < 127; address++ ) {
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
     // a device did acknowledge to the address.
     Serial.println (address);
     Wire.beginTransmission(address);
-    Serial.print("Scanning address ");
-    error = Wire.endTransmission();
-    Serial.println("Success");
+
+    // MY ADDITION: 
+    // This line could fail if there is an issue with any I2C device
+    // The surrounding print statements are to check if this call ever finishes
+    Serial.print("Scanning address ");  
+    error = Wire.endTransmission();  
+    Serial.println("Success");  // My addition
  
-    if (error == 0)
-    {
+    if (error == 0) {
       Serial.print("I2C device found at address 0x");
       if (address<16)
         Serial.print("0");
@@ -68,15 +47,13 @@ void loop()
       Serial.println("  !");
  
       nDevices++;
-    }
-    else if (error==4)
-    {
+    } else if (error==4) {
       Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
+      if (address<16) Serial.print("0");
       Serial.println(address,HEX);
-    }    
+    }
   }
+
   if (nDevices == 0)
     Serial.println("No I2C devices found\n");
   else
